2e12f9f54a3ce8f3a280df90c72ab78d3762f056
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-29 16:19:59 -0600

    created migration file and defined table for createReviewsTable

---
+++ src/db/migrations/20240229161447_createReviewsTable.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("reviews", (table) => {
+        table.increments("review_id").primary(); // sets the review_id as a primary key. unique ID
+        table.text("content"); // content of the review, written in markdown
+        table.integer("score"); // a numerical representation of the score given to the movie by the critic
+        table
+            .foreign("critic_id"); // foreign key | a reference ID to a particular critic
+            .foreign("movie_id"); // foreign key | a reference ID to a particular movie
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("reviews");
+};
