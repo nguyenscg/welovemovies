4dd4d0466364494c9d330359ed1c73d48c3a1ec8
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-29 16:13:59 -0600

    created migration file and defined table for createMoviesTable

---
+++ src/db/migrations/20240229160740_createMoviesTable.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("movies", (table) => {
+        table.incremenets("movies_id").primary(); // sets 'movies_id' as primary key; unique ID
+        table.string("title"); // the title of the movie. data type: string
+        table.integer("runtime_in_minutes"); // length of the movie in minutes. data type: integer
+        table.string("rating"); // rating given to the movie. data type: string
+        table.string("description"); // a shortened description of the movie. data type: string
+        table.string("image_url"); // a url to the movie's poster. data type: string
+        table.timestamps(true, true);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("movies");
+};
