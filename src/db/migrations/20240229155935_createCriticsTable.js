7094159817dc7aa294f606b912b0b89497102c97
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-29 16:07:12 -0600

    define the critics table

--- src/db/migrations/20240229155935_createCriticsTable.js
+++ src/db/migrations/20240229155935_createCriticsTable.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("critics", (table) => {
+    table.increments("critics_id").primary(); // sets critics_id as primary key, unique id
+    table.string("preferred_name"); // the critic's preferred first name that has a data type of string
+    table.string("surname"); // the critic's last name that has a data type of string
+    table.string("organization_name"); // the name of the organization the critic works for. data type is string
+    table.timestamps(true, true); // add 'created_at' and 'updated_at' columns. this will help keep track of the table's records.
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex) { 
+    return knex.schema.dropTable("critics"); // drop critics table if exports.down gets invoked when you undo migrations
 };
