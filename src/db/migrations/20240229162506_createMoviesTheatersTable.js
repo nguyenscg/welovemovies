d2e6eb9fcc0761a3942b713f1bb8e6296a97dbd6
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-29 16:34:04 -0600

    created migration file and defined table for createMoviesTheatersTable

---
+++ src/db/migrations/20240229162506_createMoviesTheatersTable.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movie_id").unsigned().notNullable(); // prevents negative values from being inserted to movie_id. movie_id cannot be null.
+    table
+        .foreign("movie_id") // create foreign key constraint: movie_id
+        .references("movie_id") // references primary key
+        .inTable("movies") // of movies table
+        .onDelete("CASCADE");
+    table.integer("theater_id").unsigned().notNullable(); // prevents negative values from being inserted to theater_id. theater_id cannot be null.
+    table
+        .foreign("theater_id") // create foreign key called theater_id
+        .references("theater_id") // references primary key 'theater_id'
+        .inTable("theaters") // of theaters table
+        .onDelete("CASCADE"); // if theater gets deleted, all movies will be deleted from database as well
+
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("movies_theaters");
+};
