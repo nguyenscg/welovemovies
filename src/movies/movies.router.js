0283279012a4bcac36da439f979648e9507c3df7
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-01 17:47:23 -0600

    rearranged routing for movies router and add get reviews and theaters route to movies

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,39 +2,22 @@ const router = require("express").Router(); // require express router instance
 const controller = require("./movies.controller"); // require movies controller
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+router // GET /movies endpoint
+  .route("/") 
+  .get(controller.list)
+  .all(methodNotAllowed);
+
+router
+  .route("/:movieId") // GET /movies/:movieId endpoint
+  .get(controller.read);
+
 router
   .route("/:movieId/theaters")
-  .get(controller.getReviews);
+  .get(controller.getTheaters);
 
 router
   .route("/:movieId/reviews")
-  .get("controller.getTheaters");
+  .get(controller.getReviews);
 
-router
-    .route("/") // GET /movies endpoint
-    .get(controller.list);
 
-router 
-    .route("/:movieId") // GET /movies/:movieId endpoint
-    .get(controller.read);
-
-// GET movies
-/* ```json
-{
-    "data": [
-      {
-        "id": 1,
-        "title": "Spirited Away",
-        "runtime_in_minutes": 125,
-        "rating": "PG",
-        "description": "Chihiro ...",
-        "image_url": "https://imdb-api.com/..."
-      }
-      // ...
-    ]
-  } 
-  */
-
-// GET /movies?is_showing=true
-// In the event where `is_showing=true` is provided, the route should return _only those movies where the movie is currently showing in theaters._ This means you will need to check the `movies_theaters` table.
 module.exports = router;
