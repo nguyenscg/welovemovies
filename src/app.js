c7ed05818516a578f34bb854839ec43d90a2e20f
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-01 17:51:11 -0600

    require express router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const router = express.Router();
+const moviesRouter = require("./movies/movies.router");
 
 
 module.exports = app;
