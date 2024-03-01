f9b453f4de3561b4cd67f429b677909242a73aea
Sophia Nguyen <nguyencsg@gmail.com>
2024-02-29 22:18:17 -0600

    require knex instance and create function to list all movies when get request is made

---
+++ src/movies/movies.service.js
@@ -0,0 +1,31 @@
+const knex = require("../db/connection"); // requires Knex instance that is initialized in db/connection.js
+
+// GET all movies
+// This route will return a list of all movies. Different query parameters will allow for limiting the data that is returned.
+function list() {
+    return knex("movies").select("*"); // knex query from movies table and return list of all movies
+}
+
+// GET movies
+/* ```json
+{
+    "data": [
+      {
+        "id": 1,
+        "title": "Spirited Away",
+        "runtime_in_minutes": 125,
+        "rating": "PG",
+        "description": "Chihiro ...",
+        "image_url": "https://imdb-api.com/..."
+      }
+      // ...
+    ]
+  } 
+  */
+
+// GET /movies?is_showing=true
+// In the event where `is_showing=true` is provided, the route should return _only those movies where the movie is currently showing in theaters._ This means you will need to check the `movies_theaters` table.
+
+module.exports = {
+    list,
+}
