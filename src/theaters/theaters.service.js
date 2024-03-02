a1ff07e4cc2d1f3b2ebd658806dcce2101e467bd
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-02 02:16:58 -0600

    fixed service knex query selecting columns

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -7,14 +7,14 @@ const knex = require("../db/connection"); // require connection file to enable k
 function list() {
     return knex("theaters") // knex query theaters table
         .select("*") // select all columns
-        .groupBy("theaters.theater_id"); // group by theater id
+        // .groupBy("theaters.theater_id"); // group by theater id
 }
 
 // This route should return all the `theaters` and, the movies playing at each theatre added to the `movies` key. This means you will need to check the `movies_theaters` table.
 function listMovies(theater_id) {
     return knex("movies_theaters as mt")
         .join("movies as m", "m.movie_id", "mt.movie_id")
-        .where({ "theater_id": theater_id })
+        .where({ theater_id: theater_id })
         .select("m*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id")
 }
 
