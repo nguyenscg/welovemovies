c245767a67bdd40c96f40c6c1f032537a14c0a3f
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-02 00:36:09 -0600

    create getCritic function to query and find critic of each review

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -3,9 +3,13 @@ const knex = require("../db/connection");
 // update /reviews/:reviewId
 // first make a read function to read the review_id
 // make an update function
+// also need to get critic to update the reviews with: so create get critic function
 
 function read(review_id) {
-
+    return knex("reviews")
+        .select("*")
+        .where({ "review_id": review_id })
+        .first();
 }
 
 function update() {
@@ -13,5 +17,6 @@ function update() {
 }
 
 module.exports = {
+    read,
     update,
 }
