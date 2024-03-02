2565152caf25b35e1fcba094ea70ac51f34b0e92
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-02 00:41:20 -0600

    created update function to update review

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -12,11 +12,31 @@ function read(review_id) {
         .first();
 }
 
-function update() {
+// get critic for each review
+function getCritic(critic_id) {
+    return knex("critics") // query critics table
+        .select("*")
+        .where({ "critic_id": critic_id })
+}
 
+/*  Hint: Since the test requires a PUT method, you can update the review in the following manner:
+```js
+const updatedReview = {
+  ...response.locals.review,
+  ...request.body.data,
+  review_id: response.locals.review.review_id,
+};
+const data = await service.update(updatedReview); */
+
+function update(updatedReview) {
+    return knex("reviews") // reviews table
+        .select("*")
+        .where({ "review_id": updatedReview.review_id })
+        .update(updatedReview, "*");
 }
 
 module.exports = {
     read,
     update,
+    getCritic,
 }
