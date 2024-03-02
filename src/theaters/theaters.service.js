33c5ac190304ee9ad8718d36bad659f32bbca595
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-02 02:48:44 -0600

    fixed typo to query movies columns 'm*' to 'm.*' instead

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -15,7 +15,7 @@ function listMovies(theater_id) {
     return knex("movies_theaters as mt")
         .join("movies as m", "m.movie_id", "mt.movie_id")
         .where({ theater_id: theater_id })
-        .select("m*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id")
+        .select("m.*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id")
 }
 
 module.exports = {
