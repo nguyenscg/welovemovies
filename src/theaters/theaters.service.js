4307de47ccb9f97f550eb3d81949a25070b9ba65
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-02 02:00:49 -0600

    created theaters service functions to list all theaters and movies playing at each theater

---
+++ src/theaters/theaters.service.js
@@ -0,0 +1,26 @@
+const knex = require("../db/connection"); // require connection file to enable knex
+
+// This route will return a list of all theaters. Different query parameters will allow for additional information to be included in the data that is returned.
+// GET /theaters
+// Get all theaters
+
+function list() {
+    return knex("theaters") // knex query theaters table
+        .select("*") // select all columns
+        .groupBy("theaters.theater_id"); // group by theater id
+}
+
+// This route should return all the `theaters` and, the movies playing at each theatre added to the `movies` key. This means you will need to check the `movies_theaters` table.
+function listMovies(theater_id) {
+    return knex("movies_theaters as mt")
+        .join("movies as m", "m.movie_id", "mt.movie_id")
+        .where({ "theater_id": theater_id })
+        .select("m*", "mt.created_at", "mt.updated_at", "mt.is_showing", "mt.theater_id")
+}
+
+module.exports = {
+    list,
+    listMovies,
+}
+
+
